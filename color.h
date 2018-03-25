dec77e4ffc4fe89e1ac4f0c9823882a177727c2f
ozw-sei <seijiro4751@gmail.com>
2018-03-26 00:17:22 +0900

    initial commit

---
+++ color.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "vector.h"
+#include "ray.h"
+
+vec3 color(const ray& r) {
+	vec3 unit_direction = unit_vector(r.direction());
+	float t = 0.5 * (unit_direction.y() + 1.0);
+	return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+}
